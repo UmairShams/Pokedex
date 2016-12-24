0a534260a701051a4cbf45a394a8ba90f6132ad2
Umair Ahmad <umairshams65@gmail.com>
2016-12-25 01:13:49 +0500

    2nd commit

---
+++ Pokedex3/PokeView.swift
@@ -0,0 +1,13 @@
+//
+//  PokeView.swift
+//  Pokedex3
+//
+//  Created by Umair Ahmad on 12/24/16.
+//  Copyright © 2016 Umair Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class PokeView: UICollectionViewCell {
+    
+}
---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Umair Ahmad on 12/24/16.
+//  Copyright © 2016 Umair Ahmad. All rights reserved.
+//
+
+import Foundation
